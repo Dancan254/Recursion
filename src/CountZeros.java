f17d0d2f34ae8e6e02f1925508aedecf9886427f
Dancan254 <dancanian25@gmail.com>
2024-10-30 10:22:08 +0300

    chore: Refactor CountZeros class to improve readability and add missing comments

--- src/CountZeros.java
+++ src/CountZeros.java
@@ -1,3 +1,4 @@
+
 public class CountZeros {
 
     public static void main(String[] args) {
@@ -5,15 +6,17 @@ public class CountZeros {
         System.out.println("count = " + count);
 
         System.out.println("countZerosWithRecursion(1020200004) = " + countZerosWithRecursion(1020200004));
+
+        System.out.println("countZerosWithRecursion2(1020200004, 0) = " + countZerosWithRecursion2(1020200004, 0));
     }
 
-    public static int countZeros(int number){
+    public static int countZeros(int number) {
         int count = 0;
-        while(number > 0){
+        while (number > 0) {
             //find the last digit
-            int last = number %10;
-            if (last == 0){
-                count ++;
+            int last = number % 10;
+            if (last == 0) {
+                count++;
             }
 
             number /= 10;
@@ -21,15 +24,27 @@ public class CountZeros {
         return count;
     }
 
-    public static int countZerosWithRecursion(int number){
-        if(number == 0){
+    public static int countZerosWithRecursion(int number) {
+        if (number == 0) {
             return 0;
         }
 
         int last = number % 10;
-        if(last == 0){
-            return 1 + countZerosWithRecursion(number/10);
+        if (last == 0) {
+            return 1 + countZerosWithRecursion(number / 10);
         }
-        return countZerosWithRecursion(number/10);
+        return countZerosWithRecursion(number / 10);
+    }
+
+    public static int countZerosWithRecursion2(int number, int count) {
+        if (number == 0) {
+            return count;
+        }
+
+        int last = number % 10;
+        if (last == 0) {
+            count++;
+        }
+        return countZerosWithRecursion2(number / 10, count);
     }
 }
