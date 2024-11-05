b978c531b1793cc7da734a5b0a74f569d04892af
Dancan254 <dancanian25@gmail.com>
2024-11-05 14:39:14 +0300

    feat: Add printTriangleRecursive method to Triangle class

--- src/patterns/Triangle.java
+++ src/patterns/Triangle.java
@@ -1,6 +1,5 @@
 
 public class Triangle {
-
     public static void main(String[] args) {
         printTriangle(5);
         printTriangleWithRecursion(5);
@@ -19,6 +18,37 @@ public class Triangle {
         printTriangleRecursive(n, 1);
     }
 
+    //n is the number of rows, i is the current col
+    /*
+     * Initial Call: printTriangleRecursive(4, 1)
+
+    i = 1
+    Print: *
+    Newline
+    Recursive Call: printTriangleRecursive(4, 2)
+    Second Call: printTriangleRecursive(4, 2)
+
+    i = 2
+    Print: * *
+    Newline
+    Recursive Call: printTriangleRecursive(4, 3)
+    Third Call: printTriangleRecursive(4, 3)
+
+    i = 3
+    Print: * * *
+    Newline
+    Recursive Call: printTriangleRecursive(4, 4)
+    Fourth Call: printTriangleRecursive(4, 4)
+
+    i = 4
+    Print: * * * *
+    Newline
+    Recursive Call: printTriangleRecursive(4, 5)
+    Base Case: printTriangleRecursive(4, 5)
+
+    i = 5 (greater than n)
+    Return and stop recursion
+     */
     public static void printTriangleRecursive(int n, int i) {
         if (i > n) {
             return;
